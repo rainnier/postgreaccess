f3782fc1c416e536e30aeb6953835a9f0aadddd0
rain <rainnier@gmail.com>
2023-02-15 03:43:39 +0800

    add Application.java and sample application.properties

---
+++ src/main/java/com/postgresaccess/PostgresaccessApplication.java
@@ -0,0 +1,13 @@
+package com.postgresaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresaccessApplication.class, args);
+	}
+
+}
